0c8d4fe3e105ef7e31d11cca88ba1d67e4b0ef9a
Alashov Berkeli <yunus.alashow@gmail.com>
2015-11-17 19:32:21 -0600

    glide instead of picasso

--- library/src/main/java/com/daimajia/slider/library/SliderTypes/TextSliderView.java
+++ library/src/main/java/com/daimajia/slider/library/SliderTypes/TextSliderView.java
@@ -11,16 +11,17 @@ import com.daimajia.slider.library.R;
 /**
  * This is a slider with a description TextView.
  */
-public class TextSliderView extends BaseSliderView{
+public class TextSliderView extends BaseSliderView {
     public TextSliderView(Context context) {
         super(context);
     }
 
     @Override
     public View getView() {
-        View v = LayoutInflater.from(getContext()).inflate(R.layout.render_type_text,null);
-        ImageView target = (ImageView)v.findViewById(R.id.daimajia_slider_image);
-        TextView description = (TextView)v.findViewById(R.id.description);
+        View v = LayoutInflater.from(getContext()).inflate(R.layout.render_type_text, null);
+        ImageView target = (ImageView) v.findViewById(R.id.daimajia_slider_image);
+        TextView description = (TextView) v.findViewById(R.id.description);
+        target.setScaleType(ImageView.ScaleType.FIT_XY);
         description.setText(getDescription());
         bindEventAndShow(v, target);
         return v;
